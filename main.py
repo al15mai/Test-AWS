ac7593b42d59eb99472b3c7414633a5133c9512d
X-MAS <al.craciun98@gmail.com>
2023-12-21 19:51:41 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import os
 
 app = FastAPI()
 
@@ -7,6 +8,19 @@ app = FastAPI()
 @app.get("/")
 def index():
     return "hello"
+
+
+@app.get("/trimite_text")
+def index(text: str):
+    with open("fisier.txt", "w") as file:
+        file.write(text)
+
+    try:
+        os.system("git add fisier.txt")
+    except Exception as e:
+        pass
+    os.system("git commit -m 'update' ")
+    os.system("git push")
 
 
 uvicorn.run(app)
